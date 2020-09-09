3185e167858483fc6cecb6eda9531b6a496bcd9a
gebremedhin85 <gebremedhin1985@gmail.com>
2020-09-09 18:08:47 +0200

    POST endpoint api/channels created

---
+++ src/server/api/controllers/channels-controller.js
@@ -0,0 +1,24 @@
+const knex = require('../../config/db');
+const Error = require('../lib/utils/http-error');
+const moment = require('moment-timezone');
+
+const createChannel = async (body) => {
+    await knex('channels').insert({
+      title: body.title,
+      created_at: moment(body.created_at).format(),
+      updated_at: moment(body.updated_at).format(),
+      deleted_at: moment(body.deleted_at).format(),
+    });
+  
+    return {
+      successful: true,
+    };
+  };
+  
+  module.exports = {
+    //getModules,
+    //getModuleById,
+    //deleteModule,
+    createChannel,
+    //editModule,
+  };
---
+++ src/server/api/routes/channels-router.js
@@ -0,0 +1,60 @@
+const express = require('express');
+
+const router = express.Router({ mergeParams: true });
+
+// controllers
+const channelsController = require('../controllers/channels-controller');
+
+/**
+ * @swagger
+ * /modules:
+ *  post:
+ *    summary: Create a channel
+ *    description:
+ *      Will create a channel.
+ *    produces: application/json
+ *    parameters:
+ *      - in: body
+ *        name: channel
+ *        description: The channel to create.
+ *        schema:
+ *          type: object
+ *          required:
+ *              - created_at         
+ *              - updated_at
+ *          properties:
+ *            title:
+ *              type: string              
+ *            created_at:
+ *              type: string
+ *              format: date-time
+ *              
+ *            updated_at:
+ *              type: string
+ *              format: date-time
+ *             
+ *            deleted_at:
+ *              type: string
+ *              format: date-time
+ *              
+ *    responses:
+ *      201:
+ *        description: Channel created
+ *      5XX:
+ *        description: Unexpected error.
+ */
+router.post('/', (req, res) => {
+    channelsController
+      .createChannel(req.body)
+      .then((result) => res.json(result))
+      .catch((error) => {
+        console.log(error);
+  
+        res
+          .status(400)
+          .send('Bad request')
+          .end();
+      });
+  });
+
+  module.exports = router;
