fc3d7816f7575a746ef816c26966a7d42842122d
gebremedhin85 <gebremedhin1985@gmail.com>
2020-09-12 17:49:16 +0200

    Requested changes made

--- src/server/api/controllers/channels-controller.js
+++ src/server/api/controllers/channels-controller.js
@@ -1,24 +1,19 @@
 const knex = require('../../config/db');
-//  const Error = require('../lib/utils/http-error');
 const moment = require('moment-timezone');
 
 const createChannel = async (body) => {
-    await knex('channels').insert({
-      title: body.title,
-      created_at: moment(body.created_at).format(),
-      updated_at: moment(body.updated_at).format(),
-      deleted_at: moment(body.deleted_at).format(),
-    });
-  
-    return {
-      successful: true,
-    };
+  await knex('channels').insert({
+    title: body.title,
+    created_at: moment(body.created_at).format(),
+    updated_at: moment(body.updated_at).format(),
+    deleted_at: moment(body.deleted_at).format(),
+  });
+
+  return {
+    successful: true,
   };
-  
-  module.exports = {
-    //  getModules,
-    //  getModuleById,
-    //  deleteModule,
-    createChannel,
-    //  editModule,
-  };
+};
+
+module.exports = {
+  createChannel,
+};
