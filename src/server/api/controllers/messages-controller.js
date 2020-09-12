677fe16cb2bc7030544169bd2589fd3d4b712978
STEP0HEN <guchy17@gmail.com>
2020-09-12 18:36:00 +0200

    fixed blank space errors

--- src/server/api/controllers/messages-controller.js
+++ src/server/api/controllers/messages-controller.js
@@ -37,7 +37,9 @@ const editMessage = async (messageId, updatedMessage) => {
 };
 
 const deleteMessage = async (messagesId) => {
-  return knex('messages').where({ id: messagesId }).del();
+  return knex('messages')
+    .where({ id: messagesId })
+    .del();
 };
 
 const createMessage = async (body) => {
