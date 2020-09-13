585df1f9777fb6791514daeb3fa900a187d6dfa0
unknown <ckmaina@gmail.com>
2020-09-13 16:05:04 +0200

    adrressed reviews comments

--- src/server/api/routes/users.router.js
+++ src/server/api/routes/users.router.js
@@ -3,58 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 // controllers
-const messagesController = require('../controllers/messages-controller');
-const usersController = require('../controllers/users-controller');
-
-/**
- * @swagger
- * /messages:
- *  get:
- *    summary: Get all messages
- *    description:
- *      Will return all messages.
- *    produces: application/json
- *    responses:
- *      200:
- *        description: Successful request
- *      5XX:
- *        description: Unexpected error.
- */
-router.get('/', (req, res, next) => {
-  messagesController
-    .getMessages()
-    .then((result) => res.json(result))
-    .catch(next);
-});
-
-/**
- * @swagger
- * /messages/{ID}:
- *  get:
- *    summary: Get module by ID
- *    description:
- *      Will return single module with a matching ID.
- *    produces: application/json
- *    parameters:
- *     - in: path
- *       name: ID
- *       schema:
- *         type: integer
- *         required: true
- *         description: The ID of the module to get
- *
- *    responses:
- *      200:
- *        description: Successful request
- *      5XX:
- *        description: Unexpected error.
- */
-router.get('/:id', (req, res, next) => {
-  messagesController
-    .getMessageById(req.params.id)
-    .then((result) => res.json(result))
-    .catch(next);
-});
+const usersController = require('../controllers/users.controller');
 
 /**
  * @swagger
@@ -70,19 +19,16 @@ router.get('/:id', (req, res, next) => {
  *        description: Create a new user.
  *        schema:
  *          type: object
- *          required: *
- *            - profileImage,
- *            - user_name, *
- *            - email,
+ *          required:
+ *            - profileImage
+ *            - user_name
+ *            - email
  *            - lastSeen
- *            - created_at
- *            - updated_at
- *            - deleted_at
  *          properties:
  *            ProfileImage:
  *              type: string
  *              format: URL
- *          user_name:
+ *            user_name:
  *              type: string
  *            email:
  *              type: string
@@ -90,14 +36,7 @@ router.get('/:id', (req, res, next) => {
  *            lastSeen:
  *              type: string
  *              format: date-time
- *          created_at:
- *              type: string
- *              format: date-time
- *           updated_at:
- *              type: string
- *              format: date-time
- *           deleted_at:
- *              type: string
+ *
  *
  *    responses:
  *      201:
