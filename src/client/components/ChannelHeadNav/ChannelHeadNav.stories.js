937d6d2d4521ccc93d7eadc85f9f1521f06d5174
Wael Haded <dr.wael85@gmail.com>
2020-09-13 11:00:49 +0200

    change img src in story

--- src/client/components/ChannelHeadNav/ChannelHeadNav.stories.js
+++ src/client/components/ChannelHeadNav/ChannelHeadNav.stories.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import ChannelHeadNav from './ChannelHeadNav';
 
-export default {
-  channelName: '2ndUserName',
-  urlBack: './chat',
-  imgUrl:
-    'https://www.worldfuturecouncil.org/wp-content/uploads/2020/06/blank-profile-picture-973460_1280-1.png',
-};
-
 export const Component = () => (
   <ChannelHeadNav
     channelName="2ndUserName"
     urlBack="./chat"
-    imgUrl="https://www.worldfuturecouncil.org/wp-content/uploads/2020/06/blank-profile-picture-973460_1280-1.png"
+    imgUrl="https://loremflickr.com/320/240"
   />
 );
