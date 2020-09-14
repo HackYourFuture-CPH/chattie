b656189cc98d6a35343958be17b2c81ffc9a466b
Wael Haded <dr.wael85@gmail.com>
2020-09-14 21:44:01 +0200

    fix the storry boke

--- src/client/components/ChannelHeadNav/ChannelHeadNav.stories.js
+++ src/client/components/ChannelHeadNav/ChannelHeadNav.stories.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ChannelHeadNav from './ChannelHeadNav';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
+export default { title: 'Channel head nav', component: ChannelHeadNav };
 export const Component = () => (
-  <ChannelHeadNav
-    channelName="2ndUserName"
-    urlBack="./chat"
-    imgUrl="https://loremflickr.com/320/240"
-  />
+  // if we call ChannelHeadNav in app we don't need to import router and switch
+  <Router>
+    <ChannelHeadNav
+      channelName="2ndUserName"
+      urlBack="/chat"
+      imgUrl="https://loremflickr.com/320/240"
+    />
+    <Switch>
+      <Route path="/chat">
+        <h1>chat</h1>
+      </Route>
+    </Switch>
+  </Router>
 );
