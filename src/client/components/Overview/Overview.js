02be40f6b5934473180107b6083b001658968d62
Malek Sh <54095122+basafilm@users.noreply.github.com>
2020-09-15 19:32:04 +0200

    profile containers added to oveview page and related Css chnaged

--- src/client/components/Overview/Overview.js
+++ src/client/components/Overview/Overview.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import './Overview.styles.css';
+import Profile from '../../containers/Profile/index';
+// import UsersList from '../UsersListComponent/UsersList';
+
 import OverviewSearchMessagesBtn from './OverviewSearchMessagesBtn';
 
 function Overview() {
@@ -7,8 +11,20 @@ function Overview() {
     <div className="overview">
       <h3 className="chatTitle">Chats</h3>
       <div className="btnAndProfile">
-        <OverviewSearchMessagesBtn />
-        <p>Profile</p>
+        <Router>
+          <Link to="/profile">Profile</Link>
+          <Switch>
+            {/* <Route>
+              <UsersList />
+            </Route> */}
+            <Route>
+              <Route exact path="/profile">
+                <Profile />
+              </Route>
+              <OverviewSearchMessagesBtn />
+            </Route>
+          </Switch>
+        </Router>
       </div>
     </div>
   );
