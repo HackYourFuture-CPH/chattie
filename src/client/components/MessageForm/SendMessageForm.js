226ee72772e98984f6eb0dcc140705d31c32a587
Aswini-D3 <reddyaswini@gmail.com>
2020-09-15 08:36:23 +0200

    fixed alt

--- src/client/components/MessageForm/SendMessageForm.js
+++ src/client/components/MessageForm/SendMessageForm.js
@@ -24,7 +24,10 @@ function SendMessageForm() {
     <footer>
       <div className="container">
         <form onSubmit={handleSubmit}>
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/List-Icon.svg/768px-List-Icon.svg.png" />
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/List-Icon.svg/768px-List-Icon.svg.png"
+            alt=""
+          />
           <input
             value={input}
             onChange={(event) => setInput(event.target.value)}
