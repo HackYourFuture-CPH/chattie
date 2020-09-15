304c6756421f81fd53cc4b9e0fce952352b12d4d
unknown <ckmaina@gmail.com>
2020-09-15 15:09:25 +0200

    POST/API Endpoint reviews done2

--- src/server/api/controllers/users.controller.js
+++ src/server/api/controllers/users.controller.js
@@ -1,16 +1,41 @@
 /* TODO: This is an example controller to illustrate a server side controller.
 Can be deleted as soon as the first real controller is added. */
 
+//const moment = require('moment-timezone');
+
 const knex = require('../../config/db');
-const moment = require('moment-timezone');
 
 const createUser = async (body) => {
+  const newUser = {
+    user_name: body.userName,
+    email: body.email,
+    profile_image: body.profileImage,
+    last_seen: new Date(),
+  };
+  await knex('users').insert(newUser);
+  return {
+    successful: true,
+    user_name: newUser.user_name,
+    email: newUser.email,
+    profile_image: newUser.profile_image,
+    last_seen: newUser.last_seen,
+  };
+};
+
+module.exports = {
+  createUser,
+};
+
+/* const createUser = async (body) => {
   await knex('users').insert({
     profileImage: body.profileImage,
     user_name: body.user_name,
     email: body.email,
     lastSeen: moment(body.lastSeen).format(),
-    created_at: moment().format(),
+    created_at: moment(body.created_at).format(),
+    updated_at: moment(body.updated_at).format(),
+    deleted_at: moment(body.deleted_at).format(),
+    classId: body.classId,
   });
 
   return {
@@ -19,5 +44,21 @@ const createUser = async (body) => {
 };
 
 module.exports = {
-  createUser,
+    createUser,
 };
+
+
+const createUser = async (body) => {
+  await knex('users').insert({
+    user_name: body.userName,
+    email: body.email,
+    profileImage: body.profileImage,
+  });
+
+  return {
+    successful: true,
+  };
+};
+module.exports = {
+  createUser,
+}; */
--- src/server/api/routes/users.router.js
+++ src/server/api/routes/users.router.js
@@ -7,40 +7,37 @@ const usersController = require('../controllers/users.controller');
 
 /**
  * @swagger
- * /users:
+ * /modules:
  *  post:
- *    summary: Create a user
+ *    summary: Create a module
  *    description:
- *      Will create a user.
+ *      Will create a module.
  *    produces: application/json
  *    parameters:
  *      - in: body
- *        name: user
- *        description: Create a new user.
+ *        name: module
+ *        description: The module to create.
  *        schema:
  *          type: object
  *          required:
- *            - profileImage
  *            - user_name
  *            - email
- *            - lastSeen
+ *            - profile_image
+ *             - last_seen
  *          properties:
- *            ProfileImage:
- *              type: string
- *              format: URL
  *            user_name:
  *              type: string
  *            email:
  *              type: string
  *              format: email
- *            lastSeen:
+ *            last_seen:
  *              type: string
  *              format: date-time
- *
- *
+ *            profile_image:
+ *              type: string
  *    responses:
  *      201:
- *        description: A new user created
+ *        description: Module created
  *      5XX:
  *        description: Unexpected error.
  */
@@ -50,12 +47,7 @@ router.post('/', (req, res) => {
     .createUser(req.body)
     .then((result) => res.json(result))
     .catch((error) => {
-      console.log(error);
-
-      res
-        .status(400)
-        .send('Bad request')
-        .end();
+      res.status(400).send('Bad request').end();
     });
 });
 
