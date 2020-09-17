ab0999bf1a572d060935faa29ca301c838dbde68
Malek Sh <54095122+basafilm@users.noreply.github.com>
2020-09-17 15:48:54 +0200

    UsersList component added to overview page

--- src/client/components/Overview/Overview.js
+++ src/client/components/Overview/Overview.js
@@ -2,29 +2,25 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import './Overview.styles.css';
 import Profile from '../../containers/Profile/index';
-// import UsersList from '../UsersListComponent/UsersList';
+import UsersList from '../UsersListComponent/UsersList';
 import Search from '../Search/Search';
-
-import OverviewSearchMessagesBtn from './OverviewSearchMessagesBtn';
 
 function Overview() {
   return (
     <div className="overview">
       <h3 className="chatTitle">Chats</h3>
       <Search />
-      {/* <UsersList /> */}
+      <UsersList />
       <div className="btn-and-profile">
         <Router>
           <Link to="/profile">Profile</Link>
           <Switch>
-            <Route>
-              <Route exact path="/profile">
-                <Profile />
-              </Route>
-              <OverviewSearchMessagesBtn />
+            <Route exact path="/profile">
+              <Profile />
             </Route>
           </Switch>
         </Router>
+        <h3 className="chatTitle">Chats</h3>
       </div>
     </div>
   );
