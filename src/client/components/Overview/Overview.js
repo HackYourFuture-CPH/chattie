4b6c1c5be1a7b3dcc883775f1917de542cc0536c
Malek Sh <54095122+basafilm@users.noreply.github.com>
2020-09-17 20:57:51 +0200

    sugestions implemented

--- src/client/components/Overview/Overview.js
+++ src/client/components/Overview/Overview.js
@@ -1,26 +1,22 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 import './Overview.styles.css';
-import Profile from '../../containers/Profile/index';
-import UsersList from '../UsersListComponent/UsersList';
+
+// user List component not working and breaking all other functionalities
+// import UsersList from '../UsersListComponent/UsersList';
 import Search from '../Search/Search';
 
 function Overview() {
   return (
     <div className="overview">
-      <h3 className="chatTitle">Chats</h3>
-      <Search />
-      <UsersList />
+      <h3 className="chat-title">Chats</h3>
+      <div className="search">
+        <Search />
+      </div>
+      <div className="users-list">{/* <UsersList /> */}</div>
+
       <div className="btn-and-profile">
-        <Router>
-          <Link to="/profile">Profile</Link>
-          <Switch>
-            <Route exact path="/profile">
-              <Profile />
-            </Route>
-          </Switch>
-        </Router>
-        <h3 className="chatTitle">Chats</h3>
+        <a href="/profile">Profile</a>
+        <a href="/chats">Chats</a>
       </div>
     </div>
   );
