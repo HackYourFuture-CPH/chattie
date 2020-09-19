bcc758b02d97dfbc57972c326a34b1da2df0b457
ajgcph2019 <annajohngeorge@gmail.com>
2020-09-19 17:53:34 +0200

    removed my componnet from App.js

--- src/client/App.js
+++ src/client/App.js
@@ -11,7 +11,6 @@ import Header from './components/NavigationHeader/NavigationHeader';
 import Profile from './containers/Profile';
 import Loader from './components/Loader/Loader';
 import { UserContext } from './context/userContext';
-import MessageList from './components/MessageList/MessageList';
 
 function App() {
   const { isAuthenticated, isLoading, user } = useAuthentication();
@@ -36,7 +35,6 @@ function App() {
           </AuthenticatedRoute>
         </Switch>
       </Router>
-      <MessageList />
     </UserContext.Provider>
   );
 }
