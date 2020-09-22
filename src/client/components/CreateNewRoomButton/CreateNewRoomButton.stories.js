b1efceb38a1e378c5ab46dbba9e8cac28ea84244
Aswini-D3 <reddyaswini@gmail.com>
2020-09-22 13:16:34 +0200

    Fixed some issue #7

--- src/client/components/CreateNewRoomButton/CreateNewRoomButton.stories.js
+++ src/client/components/CreateNewRoomButton/CreateNewRoomButton.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import CreateNewRoomButton from './CreateNewRoomButton/CreateNewRoomButton';
+import CreateNewRoomButton from './CreateNewRoomButton';
 
 export default { title: 'CreateNewRoomButton' };
 
