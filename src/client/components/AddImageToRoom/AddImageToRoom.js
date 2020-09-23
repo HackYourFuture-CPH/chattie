17650d9a3d02ef9bd0781de6ffa6867ec600cf07
Lucy Chyzhova <liudmyla.chyzhova@gmail.com>
2020-09-23 14:31:15 +0200

    Now possible add an image and see this image

--- src/client/components/AddImageToRoom/AddImageToRoom.js
+++ src/client/components/AddImageToRoom/AddImageToRoom.js
@@ -1,11 +1,43 @@
 import React from 'react';
-
 import './AddImageToRoom.styles.css';
 
 export default function AddImageToRoom() {
+  const uploadedImage = React.useRef(null);
+  const imageUploader = React.useRef(null);
+
+  const handleImageUpload = (e) => {
+    const [file] = e.target.files;
+    if (file) {
+      const reader = new FileReader();
+      const { current } = uploadedImage;
+      current.file = file;
+      reader.onload = (onload) => {
+        current.src = onload.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <div className="add-image-to-room">
-      <input type="file" accept="image/*" multiple="false" />
+      <input
+        type="file"
+        accept="image/*"
+        onChange={handleImageUpload}
+        ref={imageUploader}
+      />
+
+      <div className="image-container ">
+        <div className="button">
+          <button type="button" onClick={() => imageUploader.current.click()}>
+            +
+          </button>
+        </div>
+
+        <div className="input-picture">
+          <img alt="room_img" ref={uploadedImage} />
+        </div>
+      </div>
     </div>
   );
 }
