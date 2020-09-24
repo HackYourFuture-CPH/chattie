13688d30ae686f3fcb597b13ae4390b8bb7d3b0c
Aswini-D3 <reddyaswini@gmail.com>
2020-09-24 14:06:08 +0200

    Changes done#7

--- src/client/components/CreateANewRoomButton/CreateANewRoomButton.stories.js
+++ src/client/components/CreateANewRoomButton/CreateANewRoomButton.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import CreateANewRoomButton from './CreateANewRoomButton/CreateANewRoomButton';
+import CreateANewRoomButton from './CreateANewRoomButton';
 
 export default { title: 'CreateANewRoomButton' };
 
