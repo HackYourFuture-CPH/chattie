4ce4bfefb1eeada3d0c3ff922ece477bbcef68a4
Yuka Fukuzawa <yfukuzawa318@gmail.com>
2020-09-27 13:28:33 +0200

    made setInterval

--- src/client/containers/ChannelMessagesView/ChannelMessagesView.js
+++ src/client/containers/ChannelMessagesView/ChannelMessagesView.js
@@ -1,20 +1,29 @@
 import React, { useState, useEffect } from 'react';
+import MessageList from '../../components/MessageList/MessageList';
+
 
 function ChannelMessagesView() {
-  const [messages, setMessages] = useState();
+  const [messages, setMessages] = useState([]);
+
+
+  const getMsg = async () => {
+    const response = await fetch('api/messages/');
+
+    const result = await response.json();
+
+    setMessages(result);
+  };
 
   useEffect(() => {
-    (async () => {
-      const response = await fetch('/api/channel_messages');
-      const result = await response.json();
-      setMessages(result);
-    })();
-  }, [messages]);
+    const interval = setInterval(getMsg, 5000);
+    return () => clearInterval(interval);
+  },[]);
 
   return (
     <ul className="container">
       {messages &&
-        messages.map(({ id, message }) => <li key={id}>{message}</li>)}
+        <MessageList messages={messages} />
+      }
     </ul>
   );
 }
