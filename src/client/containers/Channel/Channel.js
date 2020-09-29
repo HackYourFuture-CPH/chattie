66fac6c8a7159580a2f45c9b3ef9c7e6358ba583
Yuka Fukuzawa <yfukuzawa318@gmail.com>
2020-09-29 12:06:16 +0200

    changed channeljs and channelmessagesview.js

--- src/client/containers/Channel/Channel.js
+++ src/client/containers/Channel/Channel.js
@@ -1,72 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-// import { UserContext } from '../../context/userContext';
-import ChannelMessagesView from '../ChannelMessagesView/ChannelMessagesView';
+import { UserContext } from '../../context/userContext';
 
-function Channel() {
-  const { channelId } = useParams();
+export default function Channel() {
+  const { id } = useParams();
   const [messages, setMessages] = useState([]);
-
-  // const getMsg = async () => {
-  //   const response = await fetch(`/api/messages?channelId=${id}`);
-  //   console.log("db", response);
-  //   const result = await response.json();
-  //   console.log("result of db", result);
-  //   setMessages(result);
-  // };
 
   useEffect(() => {
     const fetchMessages = async () => {
       try {
-        const url = `/api/messages?channelId=${channelId}`;
-        const response = await fetch(url);
-        console.log('get response', response);
-        const matchedChannel = await response.json();
-        console.log('get channel id', matchedChannel);
-
+        const url = `/api/messages?channel_id=${id}`;
+        const matchedChannel = await fetch(url).then((res) => res.json());
         setMessages(matchedChannel);
       } catch (err) {
         return <p>{err}</p>;
       }
-
-      // const interval = setInterval(getMsg, 5000);
-      // return () => clearInterval(interval);
     };
     fetchMessages();
-  }, [channelId]);
-
-  // useEffect(() => {
-  //     getMsg();
-  //   const interval = setInterval(getMsg, 5000);
-  //   return () => clearInterval(interval);
-  // },[]);
-
-  return (
-    <div>
-      <ChannelMessagesView messages={messages} />
-      <ul className="message-list">
-        {messages.map(({ id, message }) => (
-          <li key={id}>{message}</li>
-        ))}
-      </ul>
-    </div>
-  );
-
-  // if (messages.length === 0) {
-  //   return (
-  //     <>
-  //       <ChannelMessagesView messages={messages}/>
-  //       <p>Messages not found for channel with id: {id}</p>
-  //       <UserContext.Consumer>
-  //         {(user) => {
-  //           const email = user ? user.email : '';
-  //           return <div>This is a private channels for the user: {email}.</div>;
-  //         }}
-  //       </UserContext.Consumer>
-  //     </>
-  //   );
-  // }
+  }, [id]);
+  if (messages.length === 0) {
+    return (
+      <>
+        <p>Messages not found for channel with id: {id}</p>
+        <UserContext.Consumer>
+          {(user) => {
+            const email = user ? user.email : '';
+            return <div>This is a private channels for the user: {email}.</div>;
+          }}
+        </UserContext.Consumer>
+      </>
+    );
+  }
   //  return console.log(messages);
 }
-
-export default Channel;
--- src/client/containers/ChannelMessagesView/ChannelMessagesView.js
+++ src/client/containers/ChannelMessagesView/ChannelMessagesView.js
@@ -3,19 +3,15 @@ import MessageList from '../../components/MessageList/MessageList';
 import PropTypes from 'prop-types';
 
 function ChannelMessagesView(props) {
-
   return (
     <ul className="container">
-      {props.messages &&
-        <MessageList messages={props.messages} />
-      }
+      {props.messages && <MessageList messages={props.messages} />}
     </ul>
   );
 }
 
 ChannelMessagesView.propTypes = {
-  messages: PropTypes.arrayOf(PropTypes.string).isRequired
-}
-
+  messages: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
 
 export default ChannelMessagesView;
