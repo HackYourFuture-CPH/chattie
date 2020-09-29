c29ca8c7078c95f5226dfe40c356ab2270267645
Lucy Chyzhova <liudmyla.chyzhova@gmail.com>
2020-09-29 15:07:26 +0200

    This story very connected to story #174 ( fixe style in index.css) (only when this task is merged you can check a result of this story) added buttons and Logo, styling

--- src/client/containers/Home/Home.js
+++ src/client/containers/Home/Home.js
@@ -1,3 +1,41 @@
 import React from 'react';
+import './Home.styles.css';
+import '../../index.css';
+import { Link } from 'react-router-dom';
+// eslint-disable-next-line import/no-useless-path-segments
+import logoAnna from '../../../client/assets/images/logoAnna.png';
 
-export const Home = () => <section className="home">Final project!</section>;
+export const Home = () => (
+  <section className="home">
+    <div className="home-containe-logo-buttons">
+      <div className="home-img">
+        <Link to="/">
+          <img className="home-logoAnna" src={logoAnna} alt="logo-anna" />
+        </Link>
+      </div>
+
+      <div className="home-buttons">
+        <div>
+          <Link to="/sign-up">
+            <button
+              className="home-buttonSignUp"
+              path
+              to="/sign-up"
+              type="button"
+            >
+              Sign up
+            </button>
+          </Link>
+        </div>
+
+        <div>
+          <Link to="/sign-in">
+            <button className="home-buttonSignIn " type="button">
+              Sign in
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  </section>
+);
