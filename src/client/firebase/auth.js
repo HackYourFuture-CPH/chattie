169ea65c9a57a63a19aacf2acae1c0e9f0b02fda
tiago <tiago86g@gmail.com>
2020-09-29 14:36:21 +0200

    update firebase user

--- src/client/firebase/auth.js
+++ src/client/firebase/auth.js
@@ -42,11 +42,12 @@ export async function signIn({ email, password }) {
 
 export async function signUp({ email, password, profileImage }) {
   try {
-    const user = await auth.createUserWithEmailAndPassword(
-      email,
-      password,
-      profileImage,
-    );
+    const user = await auth.createUserWithEmailAndPassword(email, password);
+    if (user) {
+      user.updateProfile({
+        photoURL: profileImage,
+      });
+    }
     return user;
   } catch (error) {
     handleAuthErrors(error);
