bd6d7eb902c7a0df0e6b693aa786762cdd770305
Lucy Chyzhova <liudmyla.chyzhova@gmail.com>
2020-10-01 16:29:54 +0200

    created Sign In frontend styling based on Mockup 10 fixed styling for Sign Up

--- src/client/components/SignIn/SignIn.js
+++ src/client/components/SignIn/SignIn.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import './SignIn.style.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleLeft } from '@fortawesome/fontawesome-free-solid';
+import { Link } from 'react-router-dom';
 
 export default function SignIn({ onSubmit }) {
   const [email, setEmail] = useState('');
@@ -11,31 +15,54 @@ export default function SignIn({ onSubmit }) {
     onSubmit({ email, password });
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="email">
-        Email:{' '}
-        <input
-          type="email"
-          name="email"
-          placeholder="Write your email"
-          value={email}
-          onChange={handleEmailInput}
-          required
-        />
-      </label>
-      <label htmlFor="password">
-        Password:{' '}
-        <input
-          type="password"
-          name="password"
-          placeholder="Write your password"
-          value={password}
-          onChange={handlePasswordInput}
-          required
-        />
-      </label>
-      <button type="submit">Sign in</button>
-    </form>
+    <div>
+      <div className="signIn-icon-title">
+        <div className="signIn-icon">
+          <Link to="/">
+            <FontAwesomeIcon icon={faAngleLeft} />
+          </Link>
+        </div>
+        <div className="signIn-title">
+          <h3>Sign In</h3>
+        </div>
+      </div>
+
+      <form onSubmit={handleSubmit}>
+        <div className="signIn">
+          <div className="signIn-inputs">
+            <div className="signIn-email">
+              <label htmlFor="email">
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email address"
+                  value={email}
+                  onChange={handleEmailInput}
+                  required
+                />
+              </label>
+            </div>
+            <div className="signIn-password">
+              <label htmlFor="password">
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={handlePasswordInput}
+                  required
+                />
+              </label>
+            </div>
+          </div>
+          <div>
+            <button className="signIn-button" type="submit">
+              Sign in
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
   );
 }
 
