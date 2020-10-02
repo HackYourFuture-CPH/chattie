6698fb3b080fab03a3bb1c5f76534b3ae5b5d86d
Lucy Chyzhova <liudmyla.chyzhova@gmail.com>
2020-10-02 13:29:02 +0200

    Added function to store room's image based on story # #66 fixed css

--- src/client/components/AddImageToRoom/AddImageToRoom.js
+++ src/client/components/AddImageToRoom/AddImageToRoom.js
@@ -1,41 +1,43 @@
-import React from 'react';
+// import React from 'react';
 import './AddImageToRoom.styles.css';
+import React, { useState } from 'react';
+import { useStorage } from '../../hooks/useStorage';
 
 export default function AddImageToRoom() {
-  const uploadedImage = React.useRef(null);
-  const imageUploader = React.useRef(null);
+  const [file, setFile] = useState(null);
+  const [error, setError] = useState(null);
+  const types = ['image/png', 'image/jpeg', 'image/jpg'];
 
   const handleImageUpload = (e) => {
-    const [file] = e.target.files;
-    if (file) {
-      const reader = new FileReader();
-      const { current } = uploadedImage;
-      current.file = file;
-      reader.onload = (onload) => {
-        current.src = onload.target.result;
-      };
-      reader.readAsDataURL(file);
+    const selectedFile = e.target.files[0];
+    if (selectedFile) {
+      if (types.includes(selectedFile.type)) {
+        setError(null);
+        setFile(selectedFile);
+      } else {
+        setFile(null);
+        setError('Please select an image file (png or jpg)');
+      }
     }
   };
+  const { url } = useStorage(file);
 
   return (
-    <div className="add-image-to-room">
-      <input
-        type="file"
-        accept="image/*"
-        onChange={handleImageUpload}
-        ref={imageUploader}
-      />
+    <div>
+      <div className="add-image-to-room">
+        <label className="add-image-to-room-label">
+          +
+          <input
+            className="add-image-to-room-input-file"
+            type="file"
+            accept="image/*"
+            onChange={handleImageUpload}
+          />
+        </label>
 
-      <div className="add-image-to-room_upload-image-button">
-        <div className="add-image-to-room_button">
-          <button type="button" onClick={() => imageUploader.current.click()}>
-            +
-          </button>
-        </div>
-
+        {error && <p>{error}</p>}
         <div className="add-image-to-room_image-preview">
-          <img alt="room_img" ref={uploadedImage} />
+          {url && <img alt="room-img" src={url} />}
         </div>
       </div>
     </div>
--- src/client/components/AddImageToRoom/AddImageToRoom.stories.js
+++ src/client/components/AddImageToRoom/AddImageToRoom.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import AddImageToRoom from './AddImageToRoom';
 
-export default { title: 'Add Image To Room' };
+export default { title: 'Add Image To Room', component: AddImageToRoom };
 
 export const Component = () => <AddImageToRoom />;
