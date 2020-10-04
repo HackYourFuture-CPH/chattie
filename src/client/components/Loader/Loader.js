7ea3dd2f5985cb1e8a043037f1ce986af21a7f43
STEP0HEN <guchy17@gmail.com>
2020-10-04 16:20:53 +0200

    made changes to component

--- src/client/components/Loader/Loader.js
+++ src/client/components/Loader/Loader.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import './Loader.styles.css';
 
-export default function Loader({ loadingText }) {
-  return <div className="loading">{loadingText}</div>;
+export default function Loader() {
+  return (
+    <div>
+      <div className="container">
+        <div className="canvas">
+          <div className="hour-glass"> </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-
-Loader.defaultProps = {
-  loadingText: 'Loading...',
-};
-
-Loader.propTypes = {
-  loadingText: PropTypes.string,
-};
