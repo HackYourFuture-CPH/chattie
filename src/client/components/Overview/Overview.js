47986a5e3d529488c5c54ea5f1fe65eb73028e58
Yuka Fukuzawa <yfukuzawa318@gmail.com>
2020-10-04 18:09:46 +0200

    added <RoomListOverview roomList={props.roomList} /> and proptypes

--- src/client/components/Overview/Overview.js
+++ src/client/components/Overview/Overview.js
@@ -2,13 +2,18 @@ import React from 'react';
 import './Overview.styles.css';
 import UserList from '../../containers/UserList/UserList';
 import Search from '../Search/Search';
+import RoomListOverview from '../RoomListOverview/RoomListOverview';
+import PropTypes from 'prop-types';
 
-function Overview() {
+function Overview(props) {
   return (
     <div className="overview">
       <h3 className="chat-title">Chats</h3>
       <div className="search">
         <Search />
+      </div>
+      <div className="room-list-overview">
+        <RoomListOverview roomList={props.roomList} />
       </div>
       <div className="users-list">
         <UserList />
@@ -20,5 +25,14 @@ function Overview() {
     </div>
   );
 }
+
+Overview.propTypes = {
+  roomList: PropTypes.arrayOf(
+    PropTypes.shape({
+      title: PropTypes.string.isRequired,
+      imageUrl: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
+};
 
 export default Overview;
