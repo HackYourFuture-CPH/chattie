ea7e5debdfea7ea421d064ac2d52758b8cc28b6c
Toni Petrina <tonipetrina@hotmail.com>
2020-10-04 13:39:57 +0200

    Fix broken tests

--- src/client/containers/Home/Home.test.js
+++ src/client/containers/Home/Home.test.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { StaticRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { Home } from './Home';
@@ -6,5 +7,11 @@ import { Home } from './Home';
 test('Home section has correct classname', () => {
   const title = 'home';
 
-  expect(render(<Home />).container.firstChild).toHaveClass(title);
+  expect(
+    render(
+      <StaticRouter>
+        <Home />
+      </StaticRouter>,
+    ).container.firstChild,
+  ).toHaveClass(title);
 });
