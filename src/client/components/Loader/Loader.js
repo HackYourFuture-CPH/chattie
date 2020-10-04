b19e93d9770c1d783ecee4ecbd4e734a57c35594
STEP0HEN <guchy17@gmail.com>
2020-10-04 19:11:33 +0200

    removed redundant outer div

--- src/client/components/Loader/Loader.js
+++ src/client/components/Loader/Loader.js
@@ -3,11 +3,9 @@ import './Loader.styles.css';
 
 export default function Loader() {
   return (
-    <div>
-      <div className="container">
-        <div className="canvas">
-          <div className="hour-glass"> </div>
-        </div>
+    <div className="container">
+      <div className="canvas">
+        <div className="hour-glass"> </div>
       </div>
     </div>
   );
