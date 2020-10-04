d7c3d58e09a228518e272b81d92278e6913d2366
tariqjavid <khadmies@yahoo.com>
2020-10-04 21:40:04 +0200

    Add people in group and little bit changes in create group

---
+++ src/client/components/AddPeopleToRoom/ChatGroup.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import './AddPeopleToRoom.css';
+import PropTypes from 'prop-types';
+
+// this  component is just use for demo ,i will remove when review it
+export default function ChatGroup({ roomId, roomName }) {
+  const [channelMembers, setChannelMembers] = useState([]);
+  useEffect(() => {
+    const url = `api/channel-members/${roomId}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((getMembers) => setChannelMembers(getMembers));
+  }, [roomId]);
+  return (
+    <>
+      <div className="container-chatgroup">
+        <div className="group-name">{roomName}</div>
+        <div className="wraper-chatgroup">
+          {channelMembers.map((user) => (
+            <div className="group-members-names">{user.user_name}</div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
+ChatGroup.propTypes = {
+  roomName: PropTypes.string.isRequired,
+  roomId: PropTypes.number.isRequired,
+};
--- src/client/containers/AddNewRoom/AddNewRoom.js
+++ src/client/containers/AddNewRoom/AddNewRoom.js
@@ -1,31 +1,61 @@
 import React, { useState } from 'react';
-
 import RoomForm from '../../components/AddNewRoom/RoomForm';
-import fetchWithAuth from '../../utils/fetchWithAuth';
+import '../../components/AddNewRoom/AddNewRoomStyle.css';
+import PropTypes from 'prop-types';
 
-const AddNewRoom = () => {
+const AddNewRoom = ({ addUsers }) => {
   const [roomName, setNewRoom] = useState('');
   const [roomId, setRoomId] = useState('');
+
   const inputChange = (e) => {
     setNewRoom(e.target.value);
   };
 
   // send data fucntion
-  const createRoom = async (data) => {
-    const response = await fetchWithAuth('api/channels', {
+  const createRoom = async (name) => {
+    const response = await fetch('api/channels', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify({ data }),
+      body: JSON.stringify({ title: name }),
     });
-    return response;
+    return response.json();
   };
   const onCreate = async (event) => {
     event.preventDefault();
     const responseFromServer = await createRoom(roomName);
     setRoomId(responseFromServer.id);
   };
+
+  // Add user of group in server
+  if (roomId !== '') {
+    addUsers.forEach((user) => {
+      fetch('api/channel-members', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ channelId: roomId, userId: user.id }),
+      });
+    });
+  } //  end  if statement
+
+  // add dispaly people in group
+  const displaypeopleInGroup = () => {
+    return addUsers.map((user) => (
+      <div className="wraper-roomform">
+        <img src={user.profile_image} alt="user pic" />
+        <div>{user.user_name}</div>
+      </div>
+    ));
+  };
+
+  // back button that is move to back app people to room page
+  const backToAddPeople = () => {
+    window.location.assign('./addpeople');
+  };
+
   return (
     <>
       <RoomForm
@@ -33,8 +63,14 @@ const AddNewRoom = () => {
         inputChange={(e) => inputChange(e)}
         roomName={roomName}
         roomId={roomId}
+        displaypeopleInGroup={displaypeopleInGroup}
+        backToAddPeople={backToAddPeople}
       />
     </>
   );
 };
+AddNewRoom.propTypes = {
+  addUsers: PropTypes.objectOf.isRequired,
+};
+
 export default AddNewRoom;
