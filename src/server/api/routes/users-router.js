63accd0fc2312fa462b1bcbbaf1e8d8da7cee069
STEP0HEN <guchy17@gmail.com>
2020-10-06 12:28:57 +0200

    fixed api/users documentation

--- src/server/api/routes/users-router.js
+++ src/server/api/routes/users-router.js
@@ -51,7 +51,7 @@ router.delete('/:id', (req, res) => {
  *         type: integer
  *         description: The numbers of users to return
  *     - in: query
- *       name: searchUserName
+ *       name: userName
  *       schema:
  *         type: string
  *         description: Get users that partially matches the specified username
@@ -61,7 +61,7 @@ router.delete('/:id', (req, res) => {
  *         type: string
  *         description: Get users that partially matches the specified profileImageUrl
  *     - in: query
- *       name: searchUserEmail
+ *       name: email
  *       schema:
  *         type: string
  *         description: Get users that partially matches the specified user-email
