a7c33c92a783d97a8f6e935f92608bcd3acddd25
VictoriaKush <victoria.v.kush@gmail.com>
2020-10-07 00:53:44 +0200

    header navigation for messages page

--- src/client/containers/Channel/Channel.js
+++ src/client/containers/Channel/Channel.js
@@ -4,6 +4,8 @@ import SendMessageForm from '../../components/MessageForm/SendMessageForm';
 import { useParams } from 'react-router-dom';
 import { UserContext } from '../../context/userContext';
 import fetchWithAuth from '../../utils/fetchWithAuth';
+import ChannelHeadNav from '../../components/ChannelHeadNav/ChannelHeadNav';
+import useFetch from '../../hooks/useFetch';
 
 const messageFetchUpdateInterval = 3000;
 
@@ -34,14 +36,20 @@ export default function Channel() {
     if (user) {
       fetchAndSetUserFromDatabase();
     }
-
     const interval = setInterval(() => {
       fetchMessages();
     }, messageFetchUpdateInterval);
     return () => clearInterval(interval);
   }, [channelId, user]);
 
+  const { response: channel } = useFetch(`/api/channels/${channelId}`);
+  const { response: channelMembers } = useFetch(
+    `api/channel-members/membersInfo?channelId=${channelId}`,
+  );
   const currentUserEmail = user ? user.email : '';
+  const notCurrentUser = channelMembers
+    ? channelMembers.filter((member) => member.email !== currentUserEmail)
+    : [];
 
   if (!userFromDatabase) {
     return (
@@ -50,18 +58,45 @@ export default function Channel() {
       </>
     );
   }
-  if (messages.length === 0) {
+  if (channel?.title === null) {
     return (
       <>
-        <div>There does not seem to be any messages here. Try sending one</div>
+        <ChannelHeadNav
+          urlBack="/overview"
+          channelName={notCurrentUser[0].userName}
+          channelId={channelId}
+          imgUrl={notCurrentUser[0].profileImage}
+        />
+        {messages.length !== 0 ? (
+          messages && (
+            <MessageList
+              messages={messages}
+              currentUserEmail={currentUserEmail}
+            />
+          )
+        ) : (
+          <>There does not seem to be any messages here. Try sending one</>
+        )}
         <SendMessageForm channelId={channelId} userId={userFromDatabase.id} />
       </>
     );
   }
   return (
     <>
-      {messages && (
-        <MessageList messages={messages} currentUserEmail={currentUserEmail} />
+      <ChannelHeadNav
+        urlBack="/overview"
+        channelName={channel.title}
+        channelId={channelId}
+      />
+      {messages.length !== 0 ? (
+        messages && (
+          <MessageList
+            messages={messages}
+            currentUserEmail={currentUserEmail}
+          />
+        )
+      ) : (
+        <>There does not seem to be any messages here. Try sending one</>
       )}
       <SendMessageForm channelId={channelId} userId={userFromDatabase.id} />
     </>
