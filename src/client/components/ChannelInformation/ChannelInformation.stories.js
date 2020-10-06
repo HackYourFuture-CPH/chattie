28e036abb4fd940afac57c8834f9180f35ebb01c
Wael Haded <dr.wael85@gmail.com>
2020-10-06 21:51:15 +0200

    add channelInfo , css and story

---
+++ src/client/components/ChannelInformation/ChannelInformation.stories.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { RenderChannelInformation } from './ChannelInnformation';
+
+export default {
+  title: 'Channel Information',
+  component: RenderChannelInformation,
+};
+export const Component = () => <RenderChannelInformation />;
--- src/client/components/ChannelInformation/ChannelInnformation.js
+++ src/client/components/ChannelInformation/ChannelInnformation.js
@@ -1,7 +1,48 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import './ChannelInformation.style.css';
 
 export const RenderChannelInformation = ({
-  headline = 'Here is channel info',
+  channelImg,
+  channelTitle,
+  members,
 }) => {
-  return <h3>{headline}</h3>;
+  return (
+    <>
+      <div className="channel-information-header">
+        <img src={channelImg} alt="Room" className="channel-information-img" />
+      </div>
+      <h3 className="channel-information-title">{channelTitle}</h3>
+      <div className="channel-information-main">
+        {members &&
+          members.map((member) => {
+            return (
+              <div>
+                <img
+                  src={member.imgUrl}
+                  alt="member"
+                  className="member-information-img"
+                />
+                <h4>{member.name}</h4>
+              </div>
+            );
+          })}
+      </div>
+    </>
+  );
+};
+RenderChannelInformation.propTypes = {
+  channelImg: PropTypes.string,
+  channelTitle: PropTypes.string,
+  members: PropTypes.arrayOf(PropTypes.object),
+};
+RenderChannelInformation.defaultProps = {
+  channelImg: 'https://loremflickr.com/320/240',
+  channelTitle: 'Room',
+  members: [
+    { name: 'Member', imgUrl: 'https://loremflickr.com/320/240' },
+    { name: 'Member', imgUrl: 'https://loremflickr.com/320/240' },
+    { name: 'Member', imgUrl: 'https://loremflickr.com/320/240' },
+    { name: 'Member', imgUrl: 'https://loremflickr.com/320/240' },
+  ],
 };
