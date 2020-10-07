64c76f5cf35794df8498f880725801c01591e10f
Wael85 <dr.warl85@gmail.com>
2020-10-07 15:26:56 +0200

    add channelinformaton component

--- src/client/components/ChannelInformation/ChannelInnformation.js
+++ src/client/components/ChannelInformation/ChannelInnformation.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import './ChannelInformation.style.css';
 
-export const RenderChannelInformation = ({
-  headline = 'Here is channel info',
-}) => {
-  return <h3>{headline}</h3>;
+export const RenderChannelInformation = ({ image, title, members }) => {
+  return (
+    <>
+      <div className="channel-information-header">
+        <img src={image} alt="Room" className="channel-information-img" />
+      </div>
+      <h3 className="channel-information-title">{title}</h3>
+      <div className="channel-information-main">
+        {members &&
+          members.map((member) => {
+            return (
+              <div>
+                <img
+                  src={member.imgUrl}
+                  alt="member"
+                  className="member-information-img"
+                />
+                <h4>{member.name}</h4>
+              </div>
+            );
+          })}
+      </div>
+    </>
+  );
+};
+RenderChannelInformation.propTypes = {
+  image: PropTypes.string,
+  title: PropTypes.string,
+  members: PropTypes.arrayOf(PropTypes.object),
+};
+RenderChannelInformation.defaultProps = {
+  image: '',
+  title: '',
+  members: [],
 };
