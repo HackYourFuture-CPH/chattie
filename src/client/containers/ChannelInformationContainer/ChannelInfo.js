0cce25292e1cfba609753c502456921b4643951e
Wael85 <dr.warl85@gmail.com>
2020-10-10 12:18:51 +0200

    add suggested changes

--- src/client/containers/ChannelInformationContainer/ChannelInfo.js
+++ src/client/containers/ChannelInformationContainer/ChannelInfo.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { RenderChannelInformation } from '../../components/ChannelInformation/ChannelInnformation';
+import fetchWithAuth from '../../utils/fetchWithAuth';
 
 export default function ChannelInfoRender() {
   const [channelTitle, setchannelTitle] = useState('');
@@ -8,19 +9,22 @@ export default function ChannelInfoRender() {
   const [members, setMembers] = useState([]);
   const { id } = useParams();
   useEffect(() => {
-    fetch(`/api/channels/${id}`)
-      .then((res) => res.json())
-      .then((res) => {
-        setchannelTitle(res[0].title);
-        if (res[0].url) {
-          setchannelImageUrl(res[0].url);
+    const fetchChannelAndMembers = async () => {
+      try {
+        const channel = await fetchWithAuth(`/api/channels/${id}`);
+        setchannelTitle(channel[0].title);
+        if (channel[0].url) {
+          setchannelImageUrl(channel[0].url);
         } else {
           setchannelImageUrl('https://loremflickr.com/320/240');
         }
-      });
-    fetch(`api/channel-members/${id}`)
-      .then((res) => res.json())
-      .then((res) => setMembers(res));
+        const membersRes = await fetchWithAuth(`api/channel-members/${id}`);
+        setMembers(membersRes);
+      } catch (error) {
+        return <h3>{error}</h3>;
+      }
+    };
+    fetchChannelAndMembers();
   }, [id]);
 
   return (
