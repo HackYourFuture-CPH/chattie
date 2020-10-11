d0f5145db2e371d5ecc6b101e1c7bae93de79c1e
VictoriaKush <victoria.v.kush@gmail.com>
2020-10-11 14:14:05 +0200

    added onGoToChatPage func.

--- src/client/components/LastMessageList/LastMessagesList.js
+++ src/client/components/LastMessageList/LastMessagesList.js
@@ -4,32 +4,37 @@ import PropTypes from 'prop-types';
 import './LastMessagesList.css';
 import { object } from '@storybook/addon-knobs';
 
-export default function LastMessageList({ lastChannels }) {
-  if (lastChannels === null) {
+export default function LastMessageList({ lastChannels, onGoToChatPage }) {
+  if (!lastChannels) {
     return <p>No channels, start a conversation to see it here</p>;
   }
 
   return (
     <ul>
-      {lastChannels
-        ? lastChannels.map(({ message, updatedAt, channelId, title }) => (
-            <li
-              key={channelId}
-              // onClick={()=>{}} //must open the channel
-              // onKeyDown={()=>{}}
-            >
-              <p>{title}</p>
-              <h6>{message}</h6>
-              <span>
-                <h6>{updatedAt}</h6>
-              </span>
-            </li>
-          ))
-        : []}
+      {lastChannels &&
+        lastChannels.map(({ message, updatedAt, channelId, title }) => (
+          <li
+            role="presentation"
+            key={channelId}
+            onClick={() => {
+              onGoToChatPage(channelId);
+            }}
+            onKeyDown={() => {
+              onGoToChatPage(channelId);
+            }}
+          >
+            <p>{title}</p>
+            <h6>{message}</h6>
+            <span>
+              <h6>{updatedAt}</h6>
+            </span>
+          </li>
+        ))}
     </ul>
   );
 }
 
 LastMessageList.propTypes = {
   lastChannels: PropTypes.arrayOf(object).isRequired,
+  onGoToChatPage: PropTypes.func.isRequired,
 };
--- src/client/containers/LastMessagesList/index.js
+++ src/client/containers/LastMessagesList/index.js
@@ -1,15 +1,20 @@
 import React, { useContext } from 'react';
 import { UserContext } from '../../context/userContext';
+import { useHistory } from 'react-router-dom';
 import useFetch from '../../hooks/useFetch';
 import LastMessagesList from '../../components/LastMessageList/LastMessagesList';
 import Loader from '../../components/Loader/Loader';
 import Error from '../../components/ErrorComponent/Error';
 
 const LastChannelsMessageList = () => {
+  const history = useHistory();
   const user = useContext(UserContext);
   const id = user ? user.id : '';
   const url = `/api/channels-message/last-messages?userId=${id}`;
   const { response: lastChannels, loading, error } = useFetch(url);
+  const onGoToChatPage = (channelId) => {
+    history.push(`/channels/${channelId}`);
+  };
   if (loading) {
     return <Loader />;
   }
@@ -18,7 +23,14 @@ const LastChannelsMessageList = () => {
     return <Error />;
   }
   return (
-    <>{lastChannels && <LastMessagesList lastChannels={lastChannels} />}</>
+    <>
+      {lastChannels && (
+        <LastMessagesList
+          lastChannels={lastChannels}
+          onGoToChatPage={onGoToChatPage}
+        />
+      )}
+    </>
   );
 };
 
