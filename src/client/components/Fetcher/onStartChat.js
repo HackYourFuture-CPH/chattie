85509d2ec90025b6179c7c85384b771c34047c2c
Yuka Fukuzawa <yfukuzawa318@gmail.com>
2020-10-13 23:34:39 +0200

    moved UserList/onStartChat.js into components

---
+++ src/client/components/Fetcher/onStartChat.js
@@ -0,0 +1,40 @@
+import { useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import fetchWithAuth from '../../utils/fetchWithAuth';
+import { UserContext } from '../../context/userContext';
+import { getExistingChannelForUsers } from '../../api/getExistingСhannelForUsers';
+
+export const OnStartChat = () => {
+  const history = useHistory();
+  const user = useContext(UserContext);
+  const onCreateConversation = async (userId, currentUser) => {
+    const {
+      existChannelId,
+      currentUserFromServer,
+    } = await getExistingChannelForUsers(currentUser, userId);
+
+    if (existChannelId?.length) {
+      history.push(`/channels/${existChannelId}`);
+    } else {
+      const { id } = await fetchWithAuth('/api/channels', {
+        method: 'POST',
+        body: JSON.stringify({ title: null, id }),
+      });
+      const newlyCreatedChannelId = id;
+      await fetchWithAuth('/api/channel-members', {
+        method: 'POST',
+        body: JSON.stringify({ channelId: newlyCreatedChannelId, userId }),
+      });
+      await fetchWithAuth('/api/channel-members', {
+        method: 'POST',
+        body: JSON.stringify({
+          channelId: newlyCreatedChannelId,
+          userId: currentUserFromServer.id,
+        }),
+      });
+
+      history.push(`/channels/${newlyCreatedChannelId}`);
+    }
+  };
+  return { user, onCreateConversation };
+};
