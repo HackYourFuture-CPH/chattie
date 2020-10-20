a3c234345e012709cdc43b92575f9671027ecb83
behuFTP <benjamin.dals.hughes@gmail.com>
2020-10-20 16:40:43 +0200

    fixed add groups

--- src/client/components/AddPeopleToRoom/DisplayUserInGroup.js
+++ src/client/components/AddPeopleToRoom/DisplayUserInGroup.js
@@ -5,17 +5,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTimesCircle } from '@fortawesome/free-solid-svg-icons';
 
 export default function DisplayUserInGroup({ addedUsers, onRemoveFromGroup }) {
-  if (addedUsers !== 0) {
+  if (addedUsers.length !== 0) {
     return addedUsers.map((user) => (
       <div className="selected-people-wraper" key={user.id + 1}>
         <img src={user.profile_image} alt="users_image" />
-        <div>{user.user_name}</div>
+        <div>{user.userName}</div>
         <div role="presentation" onClick={() => onRemoveFromGroup(user.id)}>
           <FontAwesomeIcon icon={faTimesCircle} className="icons-color" />
         </div>
       </div>
     ));
   }
+  return null;
 }
 DisplayUserInGroup.propTypes = {
   addedUsers: PropTypes.arrayOf(Object).isRequired,
