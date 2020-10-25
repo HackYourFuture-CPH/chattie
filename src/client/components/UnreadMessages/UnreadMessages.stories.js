cdc273a0604b32308786c3d910e6f9071993b28e
Malek Sh <54095122+basafilm@users.noreply.github.com>
2020-10-25 23:31:50 +0100

    unreadMessages stories corrected

--- src/client/components/UnreadMessages/UnreadMessages.stories.js
+++ src/client/components/UnreadMessages/UnreadMessages.stories.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import UpdateUnreadMessages from './UpdateUnreadMessages';
-import CountUnreadMessages from './CountUnreadMessages';
 
 export default {
   title: 'CountUnreadMessages',
   components: {
-    component1: UpdateUnreadMessages,
-    component2: CountUnreadMessages,
+    component: UpdateUnreadMessages,
   },
 };
 
-export const Component1 = () => <UpdateUnreadMessages />;
-export const Component2 = () => <CountUnreadMessages />;
+export const Component = () => <UpdateUnreadMessages />;
